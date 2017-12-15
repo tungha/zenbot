11b3863c6ed0333cf6532b924f3c8ad48e6e6cbe
tungha <tungbeng2006@gmail.com>
2017-12-15 20:42:37 +0700

    test strategy

--- extensions/strategies/ta_macd/strategy.js
+++ extensions/strategies/ta_macd/strategy.js
@@ -1,86 +1,113 @@
-var z = require('zero-fill')
-  , n = require('numbro')
+var z = require('zero-fill'),
+	n = require('numbro')
 
-module.exports = function container (get, set, clear) {
-  return {
-    name: 'ta_macd',
-    description: 'Buy when (MACD - Signal > 0) and sell when (MACD - Signal < 0).',
+module.exports = function container(get, set, clear) {
+	var lastDelta = 0;
+	return {
+		name: 'ta_ema',
+		description: 'Buy when (EMA - last(EMA) > 0) and sell when (EMA - last(EMA) < 0). Optional buy on low RSI.',
 
-    getOptions: function () {
-      this.option('period', 'period length', String, '1h')
-      this.option('min_periods', 'min. number of history periods', Number, 52)
-      this.option('ema_short_period', 'number of periods for the shorter EMA', Number, 12)
-      this.option('ema_long_period', 'number of periods for the longer EMA', Number, 26)
-      this.option('signal_period', 'number of periods for the signal EMA', Number, 9)
-      this.option('up_trend_threshold', 'threshold to trigger a buy signal', Number, 0)
-      this.option('down_trend_threshold', 'threshold to trigger a sold signal', Number, 0)
-      this.option('overbought_rsi_periods', 'number of periods for overbought RSI', Number, 25)
-      this.option('overbought_rsi', 'sold when RSI exceeds this value', Number, 70)
-    },
+		getOptions: function() {
+			this.option('period', 'period length', String, '15m')
+			this.option('min_periods', 'min. number of history periods', Number, 52)
+			this.option('trend_ema', 'number of periods for trend EMA', Number, 2)
+			this.option('neutral_rate', 'avoid trades if abs(trend_ema) under this float (0 to disable, "auto" for a variable filter)', Number, 0)
+			this.option('oversold_rsi_periods', 'number of periods for oversold RSI', Number, 20)
+			this.option('oversold_rsi', 'buy when RSI reaches this value', Number, 0)
+		},
 
-    calculate: function (s) {
-      if (s.options.overbought_rsi) {
-        // sync RSI display with overbought RSI periods
-        s.options.rsi_periods = s.options.overbought_rsi_periods
-        get('lib.rsi')(s, 'overbought_rsi', s.options.overbought_rsi_periods)
-        if (!s.in_preroll && s.period.overbought_rsi >= s.options.overbought_rsi && !s.overbought) {
-          s.overbought = true
-          if (s.options.mode === 'sim' && s.options.verbose) console.log(('\noverbought at ' + s.period.overbought_rsi + ' RSI, preparing to sold\n').cyan)
-        }
-      }
+		calculate: function(s) {
+			get('lib.ta_ema')(s, 'trend_ema', s.options.trend_ema)
+				/*
+				      if (s.options.oversold_rsi) {
+				        // sync RSI display with oversold RSI periods
+				        s.options.rsi_periods = s.options.oversold_rsi_periods
+				        get('lib.rsi')(s, 'oversold_rsi', s.options.oversold_rsi_periods)
+				        if (!s.in_preroll && s.period.oversold_rsi <= s.options.oversold_rsi && !s.oversold && !s.cancel_down) {
+				          s.oversold = true
+				          if (s.options.mode !== 'sim' || s.options.verbose) console.log(('\noversold at ' + s.period.oversold_rsi + ' RSI, preparing to buy\n').cyan)
+				        }
+				      }
+				      if (s.period.trend_ema && s.lookback[0] && s.lookback[0].trend_ema) {
+				        s.period.trend_ema_rate = (s.period.trend_ema - s.lookback[0].trend_ema) / s.lookback[0].trend_ema * 100
+				      }
+				      if (s.options.neutral_rate === 'auto') {
+				        get('lib.stddev')(s, 'trend_ema_stddev', Math.floor(s.options.trend_ema / 2), 'trend_ema_rate')
+				      }
+				      else {
+				        s.period.trend_ema_stddev = s.options.neutral_rate
+				      }
+				*/
+		},
 
-      // compture MACD
-      /*get('lib.ema')(s, 'ema_short', s.options.ema_short_period)
-      get('lib.ema')(s, 'ema_long', s.options.ema_long_period)
-      if (s.period.ema_short && s.period.ema_long) {
-        s.period.macd = (s.period.ema_short - s.period.ema_long)
-        get('lib.ema')(s, 'signal', s.options.signal_period, 'macd')
-        if (s.period.signal) {
-          s.period.macd_histogram = s.period.macd - s.period.signal
-        }
-      }*/
-      get('lib.ta_macd')(s,'macd','macd_histogram','macd_signal',s.options.ema_long_period,s.options.ema_short_period,s.options.signal_period)
-    },
+		onPeriod: function(s, cb) {
+			/*
+			      if (!s.in_preroll && typeof s.period.oversold_rsi === 'number') {
+			        if (s.oversold) {
+			          s.oversold = false
+			          s.trend = 'oversold'
+			          s.signal = 'buy'
+			          s.cancel_down = true
+			          return cb()
+			        }
+			      }
 
-    onPeriod: function (s, cb) {
-      if (!s.in_preroll && typeof s.period.overbought_rsi === 'number') {
-        if (s.overbought) {
-          s.overbought = false
-          s.trend = 'overbought'
-          s.signal = 'sold'
-          return cb()
-        }
-      }
+			      if (typeof s.period.trend_ema_stddev === 'number') {
+			        if (s.period.trend_ema_rate > s.period.trend_ema_stddev) {
+			          if (s.trend !== 'up') {
+			            s.acted_on_trend = false
+			          }
+			          s.trend = 'up'
+			          s.signal = !s.acted_on_trend ? 'buy' : null
+			          s.cancel_down = false
+			        }
+			        else if (!s.cancel_down && s.period.trend_ema_rate < (s.period.trend_ema_stddev * -1)) {
+			          if (s.trend !== 'down') {
+			            s.acted_on_trend = false
+			          }
+			          s.trend = 'down'
+			          s.signal = !s.acted_on_trend ? 'sell' : null
+			        }
+			      }
+			*/
 
-      if (typeof s.period.macd_histogram === 'number' && typeof s.lookback[0].macd_histogram === 'number') {
-        if ((s.period.macd_histogram - s.options.up_trend_threshold) > 0 && (s.lookback[0].macd_histogram - s.options.up_trend_threshold) <= 0) {
-          s.signal = 'buy';
-        } else if ((s.period.macd_histogram + s.options.down_trend_threshold) < 0 && (s.lookback[0].macd_histogram + s.options.down_trend_threshold) >= 0) {
-          s.signal = 'sell';
-        } else {
-          s.signal = null;  // hold
-        }
-      }
-      cb()
-    },
+			if (s.period.trend_ema && s.lookback[0] && s.lookback[0].trend_ema) {
+				var delta = s.period.trend_ema - s.lookback[0].trend_ema;
+				var sign = delta * lastDelta;
+				if (sign < 0) {
+					if (delta > 0) {
+						s.signal = 'buy'
+					} else {
+						s.signal = 'sell'
+					}
+				}
 
-    onReport: function (s) {
-      var cols = []
-      if (typeof s.period.macd_histogram === 'number') {
-        var color = 'grey'
-        if (s.period.macd_histogram > 0) {
-          color = 'green'
-        }
-        else if (s.period.macd_histogram < 0) {
-          color = 'red'
-        }
-        cols.push(z(8, n(s.period.macd_histogram).format('+00.0000'), ' ')[color])
-        cols.push(z(8, n(s.period.overbought_rsi).format('00'), ' ').cyan)
-      }
-      else {
-        cols.push('         ')
-      }
-      return cols
-    }
-  }
+				lastDelta = delta;
+			}
+			cb()
+		},
+
+		onReport: function(s) {
+			var cols = []
+			if (typeof s.period.trend_ema_stddev === 'number') {
+				var color = 'grey'
+				if (s.period.trend_ema_rate > s.period.trend_ema_stddev) {
+					color = 'green'
+				} else if (s.period.trend_ema_rate < (s.period.trend_ema_stddev * -1)) {
+					color = 'red'
+				}
+				cols.push(z(8, n(s.period.trend_ema_rate).format('0.0000'), ' ')[color])
+				if (s.period.trend_ema_stddev) {
+					cols.push(z(8, n(s.period.trend_ema_stddev).format('0.0000'), ' ').grey)
+				}
+			} else {
+				if (s.period.trend_ema_stddev) {
+					cols.push('                  ')
+				} else {
+					cols.push('         ')
+				}
+			}
+			return cols
+		}
+	}
 }
