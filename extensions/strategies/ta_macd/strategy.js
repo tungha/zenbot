23d6f6cc108a62cd844e1c14eefb81f2acb5d2c6
tungha <tungbeng2006@gmail.com>
2017-12-16 21:35:44 +0700

    up

--- extensions/strategies/ta_macd/strategy.js
+++ extensions/strategies/ta_macd/strategy.js
@@ -96,7 +96,8 @@ module.exports = function container(get, set, clear) {
                         if (s.period.trend_ema > s.period.trend_ema_fast && s.period.trend_ema > s.period.trend_ema_slow) {
                             var d1 = 100 * ((s.period.trend_ema - s.period.trend_ema_fast) / s.period.trend_ema_fast);
                             var d2 = 100 * ((s.period.trend_ema - s.period.trend_ema_slow) / s.period.trend_ema_slow);
-                            if (d1 > 0.2 && d2 > 0.2) {
+                            //if (d1 > 0.2 && d2 > 0.2) 
+                            {
                                 s.signal = 'sell'
                             }
                         }
