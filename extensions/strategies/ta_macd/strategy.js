ea95a16b94d612ecd8857d2a90122e27baeacd62
tungha <tungbeng2006@gmail.com>
2017-12-16 10:51:19 +0700

    test

--- extensions/strategies/ta_macd/strategy.js
+++ extensions/strategies/ta_macd/strategy.js
@@ -1,23 +1,25 @@
 var z = require('zero-fill'),
-	n = require('numbro')
+    n = require('numbro')
 
 module.exports = function container(get, set, clear) {
-	var lastDelta = 0;
-	return {
-		name: 'ta_ema',
-		description: 'Buy when (EMA - last(EMA) > 0) and sell when (EMA - last(EMA) < 0). Optional buy on low RSI.',
+    return {
+        name: 'ta_ema',
+        description: 'Buy when (EMA - last(EMA) > 0) and sell when (EMA - last(EMA) < 0). Optional buy on low RSI.',
 
-		getOptions: function () {
-			this.option('period', 'period length', String, '15m')
-			this.option('min_periods', 'min. number of history periods', Number, 52)
-			this.option('trend_ema', 'number of periods for trend EMA', Number, 2)
-			this.option('neutral_rate', 'avoid trades if abs(trend_ema) under this float (0 to disable, "auto" for a variable filter)', Number, 0)
-			this.option('oversold_rsi_periods', 'number of periods for oversold RSI', Number, 20)
-			this.option('oversold_rsi', 'buy when RSI reaches this value', Number, 0)
-		},
+        lastLastEma: 0,
+        lastEma: 0,
 
-		calculate: function (s) {
-			get('lib.ema')(s, 'trend_ema', s.options.trend_ema)
+        getOptions: function () {
+            this.option('period', 'period length', String, '15m')
+            this.option('min_periods', 'min. number of history periods', Number, 52)
+            this.option('trend_ema', 'number of periods for trend EMA', Number, 2)
+            this.option('neutral_rate', 'avoid trades if abs(trend_ema) under this float (0 to disable, "auto" for a variable filter)', Number, 0)
+            this.option('oversold_rsi_periods', 'number of periods for oversold RSI', Number, 20)
+            this.option('oversold_rsi', 'buy when RSI reaches this value', Number, 0)
+        },
+
+        calculate: function (s) {
+            get('lib.ema')(s, 'trend_ema', s.options.trend_ema)
 			/*
 						if (s.options.oversold_rsi) {
 							// sync RSI display with oversold RSI periods
@@ -38,10 +40,10 @@ module.exports = function container(get, set, clear) {
 							s.period.trend_ema_stddev = s.options.neutral_rate
 						}
 			*/
-			console.log("new ema: " + s.period.trend_ema);
-		},
+            console.log("new ema: " + s.period.trend_ema);
+        },
 
-		onPeriod: function (s, cb) {
+        onPeriod: function (s, cb) {
 			/*
 			      if (!s.in_preroll && typeof s.period.oversold_rsi === 'number') {
 			        if (s.oversold) {
@@ -71,61 +73,56 @@ module.exports = function container(get, set, clear) {
 			        }
 			      }
 			*/
-			console.log("on period: %j", s.period);
-			if (!s.in_preroll) {
-				s.signal = null;  // hold
-				if (s.period.trend_ema && s.lookback[0] && s.lookback[0].trend_ema) {
-					var delta = s.period.trend_ema - s.lookback[0].trend_ema;
-					if (lastDelta === 0)
-						lastDelta = delta;
-					var sign = delta * lastDelta;
-					if (sign < 0) {
-						if (delta > 0) {
-							console.log("decide to buy %j", {
-								lastDelta: lastDelta,
-								delta: delta,
-								currentEma: s.period.trend_ema,
-								lastEma: s.lookback[0].trend_ema
-							});
-							s.signal = 'buy'
-						} else {
-							s.signal = 'sell'
-							console.log("decide to sell %j", {
-								lastDelta: lastDelta,
-								delta: delta,
-								currentEma: s.period.trend_ema,
-								lastEma: s.lookback[0].trend_ema
-							});
-						}
-					}
+            console.log("on period: %j", s.period);
+            if (!s.in_preroll) {
+                s.signal = null;  // hold
+                if (s.period.trend_ema) {
+                    if (this.lastLastEma > this.lastEma && this.lastEma < s.period.trend_ema) {
+                        console.log("decide to buy %j", {
+                            lastEma: this.lastEma,
+                            lastLastEma: this.lastLastEma,
+                            currentEma: s.period.trend_ema,
+                            lastEma: s.lookback[0].trend_ema
+                        });
+                        s.signal = 'buy'
+                    } else if (this.lastLastEma < this.lastEma && this.lastEma > s.period.trend_ema) {
+                        s.signal = 'sell'
+                        console.log("decide to buy %j", {
+                            lastEma: this.lastEma,
+                            lastLastEma: this.lastLastEma,
+                            currentEma: s.period.trend_ema,
+                            lastEma: s.lookback[0].trend_ema
+                        });
+                    }
 
-					lastDelta = delta;
-				}
-			}
-			cb()
-		},
+                    this.lastLastEma = this.lastEma;
+                    this.lastEma = s.period.trend_ema;
+                }
+            }
+            cb()
+        },
 
-		onReport: function (s) {
-			var cols = []
-			if (typeof s.period.trend_ema_stddev === 'number') {
-				var color = 'grey'
-				if (s.period.trend_ema_rate > s.period.trend_ema_stddev) {
-					color = 'green'
-				} else if (s.period.trend_ema_rate < (s.period.trend_ema_stddev * -1)) {
-					color = 'red'
-				}
-				cols.push(z(8, n(s.period.trend_ema_rate).format('0.0000'), ' ')[color])
-				if (s.period.trend_ema_stddev) {
-					cols.push(z(8, n(s.period.trend_ema_stddev).format('0.0000'), ' ').grey)
-				}
-			} else {
-				if (s.period.trend_ema_stddev) {
-					cols.push('                  ')
-				} else {
-					cols.push('         ')
-				}
-			}
-			return cols
-		}
-	}
+        onReport: function (s) {
+            var cols = []
+            if (typeof s.period.trend_ema_stddev === 'number') {
+                var color = 'grey'
+                if (s.period.trend_ema_rate > s.period.trend_ema_stddev) {
+                    color = 'green'
+                } else if (s.period.trend_ema_rate < (s.period.trend_ema_stddev * -1)) {
+                    color = 'red'
+                }
+                cols.push(z(8, n(s.period.trend_ema_rate).format('0.0000'), ' ')[color])
+                if (s.period.trend_ema_stddev) {
+                    cols.push(z(8, n(s.period.trend_ema_stddev).format('0.0000'), ' ').grey)
+                }
+            } else {
+                if (s.period.trend_ema_stddev) {
+                    cols.push('                  ')
+                } else {
+                    cols.push('         ')
+                }
+            }
+            return cols
+        }
+    }
 }
